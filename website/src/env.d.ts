15f05462249639144d451f7b60cb642a98c10b20
heavy-lifting <lucy.cheesman@gmail.com>
2024-06-02 18:21:40 +0100

    strudel

---
+++ website/src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="vite-plugin-pwa/info" />
+/// <reference types="vite-plugin-pwa/client" />
+
+declare module 'date-fns';
